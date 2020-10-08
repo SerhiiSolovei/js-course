5c07fd247d786149dba2bd600dfd4dc329c37f4d
Your Name <you@example.com>
2020-10-08 21:19:31 +0300

    Examples of using console methods

---
+++ lessons/console/script.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+console.time("Time");
+console.table({ a: 1, b: 2, c: 3, d: 4 });
+console.error("This message is error");
+console.warn("This is warning message");
+let obj = { a: 1, b: 2, c: 3, d: 4 };
+console.info(obj);
+console.timeEnd("Time");
