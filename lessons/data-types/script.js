3eef372a12a302006ba569784bf708680cc6f606
Your Name <you@example.com>
2020-10-08 22:36:27 +0300

    Data Types Examples

---
+++ lessons/data-types/script.js
@@ -0,0 +1,39 @@
+// PRIMITIVE
+
+// String
+const name = "Serhii";
+console.log(typeof name, name);
+
+// Number
+const age = 23;
+console.log(typeof age, age);
+
+// Boolean
+const hasCat = true;
+console.log(typeof hasCat, hasCat);
+
+// null
+const car = null;
+console.log(typeof car, car);
+
+//undefined
+let test;
+console.log(typeof test, test);
+
+// REFERENCE TYPES - objects
+
+// Array
+const hobbies = ["music", "football", "cars"];
+console.log(typeof hobbies, hobbies);
+
+// Object literal
+const address = {
+  country: "Ukraine",
+  city: "Kyiv",
+};
+
+console.log(typeof address, address);
+
+// Date
+const today = new Date();
+console.log(typeof today, today);
