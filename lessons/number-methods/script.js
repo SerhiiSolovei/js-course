9d067728b371050473b2a30825b36444217c16b9
Your Name <you@example.com>
2020-10-09 20:24:06 +0300

    Number Methods Examples

---
+++ lessons/number-methods/script.js
@@ -0,0 +1,28 @@
+// Numbers
+
+const num1 = 120;
+const num2 = 50;
+let val;
+
+// Basic operations with numbers
+
+val = num1 + num2; // 170
+val = num1 - num2; // 70
+val = num1 * num2; // 6000
+val = num1 / num2; // 2.4
+val = num1 % num2; // 20
+
+// Number Methods
+val = Math.PI; // 3.141592653589793
+val = Math.E; // 2.718281828459045
+val = Math.round(2.4); // 2
+val = Math.ceil(2.4); // 3
+val = Math.floor(2.7); // 2
+val = Math.sqrt(144); // 12
+val = Math.pow(2,5); // 32
+val = Math.min(2,5,19,4,35,74,-1); // -1
+val = Math.max(2,5,19,4,35,74,-1); // 74
+
+val = Math.random(); // Random number between 0 and 1
+
+val = Math.floor(Math.random() * 100 + 1); // Random number between 1 and 100
