1cdeff1c3e1ff3a6b76316077b01909d8ad1c5c6
Your Name <you@example.com>
2020-10-16 20:49:57 +0300

    Task List Project: Delete & Filter Tasks

--- projects/task-list-project/script.js
+++ projects/task-list-project/script.js
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners () {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Add remove task event
+  taskList.addEventListener('click', removeTask);
+  // Add clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  // Add filter task event
+  filter.addEventListener('keyup', filterTasks);
 };
 
 // Add Task
@@ -39,6 +45,42 @@ function addTask (e) {
   // Append li to ul
   taskList.appendChild(li);
 
+  // Clear input
   taskInput.value = "";
-  e.preventDefault;
+
+  e.preventDefault();
+};
+
+// Remove Task
+function removeTask (e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+};
+
+// Clear Tasks
+function clearTasks () {
+  // taskList.innerHTML = '';
+
+  // Faster way
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+};
+
+// Filter Tasks
+function filterTasks (e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function (task){
+    const item = task.firstChild.textContent;
+
+    if (item.toLowerCase().indexOf(text) !== -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 };
