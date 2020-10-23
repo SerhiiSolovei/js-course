eb431f6d0707dbb3b8d550bb63d4b80b4c9761f2
Your Name <you@example.com>
2020-10-23 23:02:27 +0300

    Prototypal Inheritance Examples

---
+++ lessons/prototypes/prototypal-inheritance/script.js
@@ -0,0 +1,40 @@
+// Person Constructor
+function Person (firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+
+// Greeting
+Person.prototype.greeting = function () {
+  return `Hello there ${this.firstName} ${this.lastName}`;
+}
+
+const person1 = new Person(`John`, `Smith`);
+
+console.log(person1.greeting());
+
+// Customer constructor
+function Customer (firstName, lastName, phone, membership) {
+  Person.call(this, firstName, lastName);
+
+  this.phone = phone;
+  this.membership = membership;
+}
+
+// Inherit the Person prototype method
+Customer.prototype = Object.create(Person.prototype);
+
+// Make customer.prototype return Customer()
+Customer.prototype.constructor = Customer;
+
+// Create customer
+const customer1 = new Customer('Mike', 'Adams', '123-456-7890', 'standard');
+
+console.log(customer1);
+
+// Customer greeting
+Customer.prototype.greeting = function () {
+  return `Hello there ${this.firstName} ${this.lastName} welcome to our company`;
+};
+
+console.log(customer1.greeting());
