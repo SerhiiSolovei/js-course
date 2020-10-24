d6fece892ef2f9e4a39cbc9f6db39c0936d7879a
Your Name <you@example.com>
2020-10-24 18:03:13 +0300

    Subclasses Examples

--- lessons/classes-and-subclasses/script.js
+++ lessons/classes-and-subclasses/script.js
@@ -40,3 +40,27 @@ console.log(mary.greeting()); // Hello there Mary Williams
 // console.log(mary.addNumbers(1,2)); // TypeError: mary.addNumbers is not a function
 
 console.log(Person.addNumbers(1,2)); // 3
+
+
+// SUBCLASSES
+
+class Customer extends Person {
+  constructor (firstName, lastName, dob, phone, membership) {
+    super(firstName, lastName, dob);
+
+    this.phone = phone;
+    this.membership = membership;
+  };
+
+  static getMembershipCost () {
+    return 500;
+  };
+};
+
+const john = new Customer ('John', 'Dawson', '10-21-1995', '123-456-7890', 'standard');
+
+console.log(john);
+console.log(john.greeting()); // Hello there John Dawson
+console.log(Customer.getMembershipCost()); // 500
+console.log(Customer.addNumbers(1,2)); // 3
+// console.log(Person.getMembershipCost()); // TypeError: Person.getMembershipCost is not a function
