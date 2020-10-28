ef51924ded6f653d56d350a4d9a03dca0ecd1f36
Your Name <you@example.com>
2020-10-28 23:04:25 +0200

    Custom HTTP Library Project: POST, PUT and DELETE Requests

--- projects/custom-http-library/script.js
+++ projects/custom-http-library/script.js
@@ -1,9 +1,53 @@
 const http = new easyHTTP();
 
-http.get('https://jsonplaceholder.typicode.com/posts1', function (err, posts) {
+// GET Posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// GET Single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function (err, post) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Create Data
+const data = {
+  title: "Custom Post",
+  body: "This is a Custom Post"
+};
+
+// POST POSTS
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function (err, posts) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   };
+// });
+
+// PUT POST (Update)
+// http.put('https://jsonplaceholder.typicode.com/posts/7', data, function (err, posts) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   };
+// });
+
+
+// DELETE POST
+http.delete('https://jsonplaceholder.typicode.com/posts/7', function (err, response) {
   if (err) {
     console.log(err);
   } else {
-    console.log(posts);
-  }
+    console.log(response);
+  };
 });
