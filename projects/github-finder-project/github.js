3c9d5113d1d1ace42a734cf62bf52da7b74a7b20
Your Name <you@example.com>
2020-11-05 21:27:33 +0200

    GitHub Finder Project: Fetch & Display Repos

--- projects/github-finder-project/github.js
+++ projects/github-finder-project/github.js
@@ -2,15 +2,21 @@ class GitHub {
   constructor() {
     this.client_id = '4c51f0449d108dd4669f';
     this.client_secret = 'f79074af8d94c82af0a646322f07e5c469139fa2';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   };
 
   async getUsers (user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile // the same as profile: profile
+      profile, // the same as profile: profile
+      repos
     }
   }
 };
--- projects/github-finder-project/ui.js
+++ projects/github-finder-project/ui.js
@@ -8,11 +8,11 @@ class UI {
     this.profile.innerHTML = `
     <div class="card card-body mb-3">
       <div class="row">
-        <div class="col md-3">
+        <div class="col-md-3">
           <img class="img-fluid mb-2" src="${user.avatar_url}">
           <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
         </div>
-        <div class="col md-9">
+        <div class="col-md-9">
           <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
           <span class="badge badge-secondary">Public Gists: ${user.gists}</span>
           <span class="badge badge-success">Followers: ${user.followers}</span>
@@ -28,8 +28,33 @@ class UI {
       </div>
     </div>
     <h3 class="page-heading mb-3">Latest Repos</h3>
-    <div id"repos"></div>
+    <div id="repos"></div>
     `;
+  };
+
+  // Show user repos
+  showRepos(repos) {
+    let output = '';
+
+    repos.forEach((repo) => {
+      output += `
+      <div class="card card-body mb-2">
+        <div class="row">
+          <div class="col-md-6">
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+          </div>
+          <div class="col-md-6">
+            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+            <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+            <span class="badge badge-success">Forks: ${repo.forks_count}</span>
+          </div>
+        </div>
+      </div>
+      `;
+    });
+
+    // Output repos
+    document.getElementById('repos').innerHTML = output;
   };
 
   // Show Alert Message
@@ -53,7 +78,7 @@ class UI {
     setTimeout(()=> {
       this.clearAlert();
     }, 3000);
-  }
+  };
 
   // Clear Alert
   clearAlert () {
@@ -61,11 +86,11 @@ class UI {
 
     if (currentAlert) {
       currentAlert.remove();
-    }
-}
+    };
+  };
 
   // Clear Profile
   clearProfile () {
     this.profile.innerHTML = '';
-  }
-}
+  };
+};
