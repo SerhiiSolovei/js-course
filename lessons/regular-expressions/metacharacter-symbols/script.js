2ad66fa8573157abe3689a87c324a277af2b61ec
Your Name <you@example.com>
2020-11-20 23:58:58 +0200

    Examples of shorthand character classes in regular expression

--- lessons/regular-expressions/metacharacter-symbols/script.js
+++ lessons/regular-expressions/metacharacter-symbols/script.js
@@ -25,13 +25,35 @@ re = /[0-9]ray/;    // Match any digit                          Example: 8ray ma
 // Braces {} - Quantifiers
 re = /Hel{2}o/i;    // Must occur exactly {m} amount of times   Example: Hello matches Hel{2}o
 re = /Hel{2,4}o/i;  // Must occur between {m} amount of times   Example: Helllo matches Hel{2,4}o
-re = /Hel{2,}o/i;   // Must occur at least {m} times             Example: Hellllllo matches Hel{2,}o
+re = /Hel{2,}o/i;   // Must occur at least {m} times            Example: Hellllllo matches Hel{2,}o
 
 // Parentheses () - Grouping
 re = /^([0-9]x){3}$/;     // Example: 3x3x3x matches ^([0-9]x){3}$
 
+// Shorthand Character Classes
+re = /\w/;      // Match Word Character - alphanumeric or _        Example: 3x3x3x matches \w
+// ["3", index: 0, input: "Hello World", groups: undefined]
+re = /\w+/;     // + = Match Word Character one or more times      Example: 3x3x3x matches \w
+// ["3x3x3x", index: 0, input: "3x3x3x", groups: undefined]
+re = /\W/;      // Match Non-Word Character                        Example: ! matches \W
+re = /\d/;      // Match any digit                                 Example: 3 matches \d
+// ["3", index: 0, input: "3", groups: undefined]
+re = /\d+/;     // Match any digit 0 or more times                 Example: 312463 matches \d
+// ["312463", index: 0, input: "3", groups: undefined]
+re = /\D/;      // Match Non-Digit Character                       Example: s matches \D
+re = /\s/;      // Match whitespace (tab or space buttons) Char    Example:  matches \s
+re = /\S/;      // Match Non-whitespace Character                  Example: s matches \S
+re = /Hell\b/i; // Word boundary
+// Example: Hello, welcome to Hell Bar! matches Hell\b ["Hell", index: 18, input: "Hello, welcome to Hell Bar!", groups: undefined]
+
+// Assertions
+re = /x(?=y)/;  // Match x only if followed by y          Example: asdasd hdfhxyok matches x(?=y)
+// ["x", index: 11, input: "asdasd hdfhxyok", groups: undefined]
+re = /x(?!y)/;  // Match x only if NOT followed by y      Example: asdasd hdfhxok matches x(?=y)
+// ["x", index: 11, input: "asdasd hdfhxok", groups: undefined]
+
 // String to match
-const str = '3x3x3x'
+const str = 'asdasd hdfhxok'
 
 //Log Results
 const results = re.exec(str);
