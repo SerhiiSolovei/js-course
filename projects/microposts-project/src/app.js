2c188373c01cd25716d40c5d8e29bdc80393ed11
Your Name <you@example.com>
2020-12-10 16:57:42 +0200

    Microposts Project: Removing Posts

--- projects/microposts-project/src/app.js
+++ projects/microposts-project/src/app.js
@@ -30,6 +30,9 @@ document.addEventListener('DOMContentLoaded', getPosts);
 // Listen for submit post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// Listen for delete post
+document.querySelector('#posts').addEventListener('click', deletePost);
+
 // Get posts
 function getPosts () {
   http.get('http://localhost:3000/posts')
@@ -54,4 +57,21 @@ function submitPost() {
       ui.clearFields();
       getPosts()})
     .catch(err => console.log(err))
+}
+
+// Delete post
+function deletePost (e) {
+  if (e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id;
+
+    if (confirm('Are you sure?')) {
+      http.delete(`http://localhost:3000/posts/${id}`)
+        .then(data => {
+          ui.showAlert('Post Deleted', 'alert alert-success');
+          getPosts();
+        })
+        .catch(err => console.log(err))
+    }
+  }
+  e.preventDefault();
 }
