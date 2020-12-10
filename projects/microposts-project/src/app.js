fa565d43e10e958d75e4ad328470b0e5f47c3016
Your Name <you@example.com>
2020-12-10 21:47:07 +0200

    Microposts Project: Post Edit State & Update

--- projects/microposts-project/src/app.js
+++ projects/microposts-project/src/app.js
@@ -33,6 +33,12 @@ document.querySelector('.post-submit').addEventListener('click', submitPost);
 // Listen for delete post
 document.querySelector('#posts').addEventListener('click', deletePost);
 
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
+// Listen for cancel
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
+
 // Get posts
 function getPosts () {
   http.get('http://localhost:3000/posts')
@@ -44,19 +50,36 @@ function getPosts () {
 function submitPost() {
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
+  const id = document.querySelector('#id').value;
 
   const data = {
     title,
     body
   }
 
-  // Create post
-  http.post('http://localhost:3000/posts', data)
-    .then(data => {
-      ui.showAlert('Post added', 'alert alert-success');
-      ui.clearFields();
-      getPosts()})
-    .catch(err => console.log(err))
+  // Validate input
+  if (title === '' || body === '') {
+    ui.showAlert('Please fill all the fields', 'alert alert-danger')
+  } else {
+    // Check for ID
+    if (id === '') {
+      // Create post
+      http.post('http://localhost:3000/posts', data)
+        .then(data => {
+          ui.showAlert('Post added', 'alert alert-success');
+          ui.clearFields();
+          getPosts()})
+        .catch(err => console.log(err))
+    } else {
+      // Update post
+      http.put(`http://localhost:3000/posts/${id}`, data)
+        .then(data => {
+          ui.showAlert('Post updated', 'alert alert-success');
+          ui.changeFormState('add');
+          getPosts()})
+        .catch(err => console.log(err))
+    }
+  }
 }
 
 // Delete post
@@ -73,5 +96,34 @@ function deletePost (e) {
         .catch(err => console.log(err))
     }
   }
+  e.preventDefault();
+}
+
+// Enable edit state
+function enableEdit (e) {
+  if (e.target.parentElement.classList.contains('edit')) {
+    const id = e.target.parentElement.dataset.id;
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+    const body = e.target.parentElement.previousElementSibling.textContent;
+
+    const data = {
+      id,
+      title,
+      body
+    }
+
+    // Fill form with current post
+    ui.fillForm(data);
+  }
+
+  e.preventDefault();
+}
+
+// Cancel edit state
+function cancelEdit (e) {
+  if (e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add');
+  }
+
   e.preventDefault();
 }
